3181ed5c4c52ea3eb906ab1b0e583e374d578f1c
ChoonKiat <choonkiat.dynp@gmail.com>
2020-05-26 01:21:15 +0800

    add-vulcan

--- reddit_scraper/quickstart.py
+++ reddit_scraper/quickstart.py
@@ -1,11 +1,10 @@
 from __future__ import print_function
 from reddit import *
-import pickle
-import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-
+import pickle
+import os.path
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
--- vulcan_scraper/vulcan.py
+++ vulcan_scraper/vulcan.py
@@ -1,15 +1,42 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
-import pandas as pd
+import requests
 
-#configure webdriver to use chrome broswer
-driver = webdriver.Chrome(executable_path="")
+#obtain the url of the site that we want to scrapt
+url = 'https://vulcanpost.com/category/news/'
+page = requests.get(url)
 
 #store the articles 
 articles =[]
 
-driver.get('https://vulcanpost.com/category/news/')
-content = driver.page_source
-print(content)
+# stores the weblink
+links = []
+
+#stores the results
+results = []
 
 
+soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.p)
+news_headlines = soup.find_all('div',class_ = 'article-excerpt')
+web_links = soup.find_all('a',class_ = 'article-list-item')
+
+print(web_links)
+
+# loop the headlines and append the headlines
+for i in news_headlines:
+    #print(i.find('p').text)
+    articles.append(i.find('p').text)
+
+for i in web_links:
+    #print(i['href'])
+    links.append('https://vulcanpost.com/'+ i['href'])
+
+
+# traverse through every news article which is the links to the articles
+# for every article, find the tag that contains the story and scrape 
+# append results into array
+
+
+for j in range(len(articles)):
+    results.append([articles[j],links[j]])
+#print(results)
