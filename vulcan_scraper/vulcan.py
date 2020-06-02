8a250595eecb9be444e733561608d0489139f096
zzz77-tech <54829103+zzz77-tech@users.noreply.github.com>
2020-06-03 01:20:32 +0800

    Update vulcan.py

--- vulcan_scraper/vulcan.py
+++ vulcan_scraper/vulcan.py
@@ -14,6 +14,12 @@ links = []
 #stores the results
 results = []
 
+#stores polluted stories
+pstories=[]
+
+#stores final stories
+stories=[]
+
 
 soup = BeautifulSoup(page.content, 'html.parser')
 #print(soup.p)
@@ -31,6 +37,23 @@ for i in web_links:
     #print(i['href'])
     links.append('https://vulcanpost.com/'+ i['href'])
 
+    for i in links:
+    page=requests.get(i)
+    soup=BeautifulSoup(page.content,'html.parser')
+    story=soup.find_all('p')
+    pstories.append(story)
+
+for i in pstories:
+    res=""
+    for p in i:
+        res+=str(p)
+    sep="Featured Image Credit"
+    rest=res.split(sep,1)[0]
+    rest=rest.replace("<p>","")
+    rest=rest.replace("</p>","")
+    stories.append(rest)    
+    
+
 
 # traverse through every news article which is the links to the articles
 # for every article, find the tag that contains the story and scrape 
@@ -38,5 +61,5 @@ for i in web_links:
 
 
 for j in range(len(articles)):
-    results.append([articles[j],links[j]])
+    results.append([articles[j],links[j],stories[j])
 #print(results)
